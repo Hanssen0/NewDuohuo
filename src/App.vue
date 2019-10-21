70b7e82b75738caf961d20067873a9cb359e69ab
handsome0hell <handsome0hell@gmail.com>
2019-10-21 08:47:44 +0800

    It's a website now
    We know who we are!

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,21 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="index">
+      <div id="index-word"v-bind:style="{height: test}">
+        <p id="index-object">We're</p>
+        <p ref="group-name" id="index-subject">Duo Huo</p>
+      </div>
+    </div>
   </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'app',
-  components: {
-    HelloWorld
+  data: function() {
+    return {test: 0};
+  }, methods: {
+  }, mounted: function() {
+    this.test = this.$refs["group-name"].offsetHeight + "px";
   }
 }
 </script>
@@ -22,7 +26,38 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  margin: 0;
+}
+#index {
+  background: #ffcdd2;
+  background-size: cover;
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+}
+#index-word {
+  display:flex;
+  align-items: center;
+  justify-content: flex-end;
+  flex-direction: column;
+}
+#index-subject,
+#index-object,
+#index-preposition {
+  margin: 0;
+  line-height: 90%;
+}
+#index-object {
+  font-size: 80px;
+  --color-grey: 80%;
+  color: rgba(var(--color-grey), var(--color-grey), var(--color-grey), 0.8);
+}
+#index-subject {
+  font-size: 250px;
+  --color-grey: 100%;
+  color: rgba(var(--color-grey), var(--color-grey), var(--color-grey), 1);
 }
 </style>
