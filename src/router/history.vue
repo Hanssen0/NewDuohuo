150efcfd2ac80cc4f98c522c2af1e9cf60406d61
handsome0hell <handsome0hell@gmail.com>
2019-11-27 20:12:21 +0800

    Infinie scrolling in history page
    It's dirty to let the whole page scrolling back in history page. Now
    it can keep scrolling in the same drection with the same speed.

--- src/router/history.vue
+++ src/router/history.vue
@@ -1,13 +1,22 @@
 <template>
   <div id="viewport-cutter">
-    <div id="background" :style="{left: position + 'px'}" ref="content"
-      @transitionend="InvertBackgroundDirection">
+    <div id="background" :style="{transform: 'translateX(' + position + 'px)',
+                                  transition: 'transform ' + transition_time +
+                                              's linear'}"
+      ref="content" @transitionend="InvertBackgroundDirection">
       <div v-for="(item, index) in projects"
         class="items" v-bind:style="{background: item.color}">
         <div class="project-images" :id="[item.image]"></div>
         <p class="title">{{item.title}}</p>
         <p class="context">{{item.context}}</p>
       </div>
+      <div v-for="(item, index) in projects"
+        class="items" v-bind:style="{background: item.color}">
+        <div class="project-images" :id="[item.image]"></div>
+        <p class="title">{{item.title}}</p>
+        <p class="context">{{item.context}}</p>
+      </div>
+<!-- Repeat once for infinite scrolling -->
     </div>
   </div>
 </template>
@@ -26,7 +35,6 @@
   align-items: flex-end;
   justify-content: flex-start;
   position: relative;
-  transition: left 5s ease-in-out;
 }
 .items {
   position: relative;
@@ -85,36 +93,42 @@
 }
 </style>
 <script>
+const kTransitionTime = 20;
 export default {
   data: function() {
     return {
       projects: new Array(),
-      position: 0 };
+      position: 0,
+      transition_time: kTransitionTime
+    };
   }, methods: {
     InvertBackgroundDirection: function() {
-      if (this.position == 0) {
-        this.position = -this.$refs["content"].offsetWidth +
-                        document.body.clientWidth;
-      }else {
+console.log("F");
+      this.transition_time = 0;
+      this.$nextTick(() => {
         this.position = 0;
-      }
+        this.$nextTick(() => {
+          this.transition_time = kTransitionTime;
+          this.position = -this.$refs["content"].offsetWidth/2;
+        });
+      });
     }
   }, mounted: function() {
     this.projects.push({ title: "黑客马拉松",
                          context: `HackFire 由多火携手计软院举办，
                                    6 支队伍在同一屋檐下肝了 20 个小时，
                                    完成了各自的作品。`,
-                         color: "#90caf9",
+                         color: "#90caf9C0",
                          image: "project-image-one" });
     this.projects.push({ title: "IDOC",
                          context: `idoc ，期末的救星，让你成为考试的豪杰。集中了相当量的各学科历年期末试卷与复习资料。`,
-                         color: "#ffcc80",
+                         color: "#ffcc80C0",
                          image: "project-image-two" });
     this.projects.push({ title: "信大百科",
                          context: `技术上已经没有问题，但正如所有wiki一样，需要有更多的人来编辑才有意义......`,
-                         color: "#a5d6a7",
+                         color: "#a5d6a7C0",
                          image: "project-image-three" });
-    this.$nextTick(() => { this.position = -this.$refs["content"].offsetWidth + document.body.clientWidth; });
+    this.$nextTick(() => { this.position = -this.$refs["content"].offsetWidth/2; });
   }
 }
 </script>
